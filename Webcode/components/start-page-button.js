6398f187a4f51e14116a01a30a62e4b1a04f5f8d
Milnerrafe <106588878+Milnerrafe@users.noreply.github.com>
2024-09-16 10:33:32 +0800

    Added comments. Tried input field.

--- Webcode/components/start-page-button.js
+++ Webcode/components/start-page-button.js
@@ -1,9 +1,11 @@
+// This JavaScript file uses a lit. Lit is a powerful JavaScript framework that helps us use web components, which is a standard for creating custom HTML components in a simple and easy to use way.
 import {
   LitElement,
   html,
   css,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
 
+// In this part of the code we are defining what attributes will be able to be put into the HTML when the component is used.
 class StartButton extends LitElement {
   static properties = {
     rectColor: { type: String, attribute: "rect-color" },
@@ -11,13 +13,14 @@ class StartButton extends LitElement {
     buttonText: { type: String, attribute: "button-text" },
   };
 
+  // Here we define the default attributes in case the HTML doesn't provide any for us.
   constructor() {
     super();
     this.rectColor = "#4E80FF"; // Default color
     this.hoverColor = "#3B6ADF"; // Default hover color
     this.buttonText = "Click Me!"; // Default button text
   }
-
+  // Here we define the CSS, or cascading style sheets. When we use lit, we don't have to worry about cascading style sheets though, because we use something called the shadow DOM, which takes the component into their own separate DOM, meaning that styles don't cascade down onto them. The CSS only applies to the elements which it renders, and no other CSS applies to it. This CSS defines how the button should look.
   static styles = css`
     .start-button {
       width: 251px;
@@ -41,6 +44,7 @@ class StartButton extends LitElement {
     }
   `;
 
+  // Here we combine the variables and the HTML that we want to render. This JavaScript also returns a finished component to the HTML to display on the screen.
   render() {
     return html`
       <div
@@ -54,6 +58,7 @@ class StartButton extends LitElement {
     `;
   }
 
+  // This code is where we add the logic to the button. When the button is clicked, depending on its name provided in the HTML, it will go to different parts of the application. If the value of the button is unrecognised, we throw an error that can be viewed in the developer console on a browser. This helps us identify errors and problems.
   handleClick() {
     if (this.buttonText == "Play Random") {
       window.location.href = "/play";
@@ -72,4 +77,5 @@ class StartButton extends LitElement {
   }
 }
 
+// This piece of JavaScript defines the element. So, when the JavaScript file is imported into an HTML file, and somebody calls or puts down the element in the HTML file, the HTML browser knows to render this component.
 customElements.define("start-page-button", StartButton);
