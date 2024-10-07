8bdd3a37b7d2d4be4a01f4ee5941dcf5ffb72dd0
Milnerrafe <106588878+Milnerrafe@users.noreply.github.com>
2024-10-07 14:53:24 +0800

    'Added more features to the Python client to fix game function is not returning WGS correctly'. 'Start page function is fix game function to properly be able to control WGS and to be able to stop the loop'.

--- PyWorldleCS/index.py
+++ PyWorldleCS/index.py
@@ -4,22 +4,61 @@ from wordle_module import clear_terminal
 from colorama import Fore, Back, Style, init
 import pyfiglet
 
+
 # Set up a basic code for variables and for managing colorama
 init(autoreset=True)
-worddata = getworldewords(0)
+wgs = -1
+worddata = getworldewords(wgs)
+v1 = 0
+aft = 0
+
+# Make a function that will serve as the start page to the Python app, telling users to press play to start the game or press C to close the game.
+def startpage():
+    i1wgs = -1
+    wcs_1_big_text = pyfiglet.figlet_format("Wordle CS", font="slant")
+    print(Fore.GREEN + Style.BRIGHT + wcs_1_big_text)
+    print(Fore.GREEN + Style.BRIGHT + "Welcome to Wordle CS, the community made infinite wordle game.")
+    print("[P]lay")
+    print("[C]ose")
+    kbp1 = input("Please choose your option, press the key and then press Enter")
+    if kbp1 == "p":
+        i1wgs = 0
+    elif kbp1 == "c":
+        i1wgs = -2
+    else:
+        print("Error with start page function")
+    return i1wgs
+
+def game():
+   clear_terminal()
+   if aft == 3:
+       i2wgs = -2
+   else:
+       i2wgs = 0
+   return i2wgs
 
 
-big_text = pyfiglet.figlet_format("Wordle CS", font="slant")
-print(Fore.GREEN + Style.BRIGHT + big_text)
 
 
-print("WordleCS")
-print("world")
-print(Fore.RED + Back.YELLOW + Style.BRIGHT + 'Red text with yellow background and bright style')
+while v1 == 0:
 
+   #debug
+   print(wgs)
+   aft = aft +1
+   print(aft)
+   print(worddata)
+   #debug
 
-# Initialize colorama
-
+   if wgs >= 0:
+       wsg = game()
+   elif wgs == -1:
+       wgs = startpage()
+   elif wgs == -2:
+       v1 = 1
+   else:
+       exit()
+else:
+    exit()
 
 
 
